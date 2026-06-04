03278393559cdfd4604b6194e483f95578d3a15d
Salman Badr <mailsbadr@gmail.com>
2026-06-04 07:53:22 -0400

    3: added a system prompt with persona:

--- src/chat.py
+++ src/chat.py
@@ -4,14 +4,24 @@ from dotenv import load_dotenv
 from openai import OpenAI, RateLimitError
 
 load_dotenv()
+persona = input("Enter persona for open ai chat below:\n")
 prompt = input("Enter prompt for open ai chat below:\n")
 
 try:
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
     response = client.responses.create(
-        model="gpt-5.4-nano",
-        input=prompt
+        model="gpt-5.4-nano", 
+        input=[
+            {
+                "role": "developer",
+                "content": persona
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
     )
 except RateLimitError as e:
     print(f"There was a problem {e}")
