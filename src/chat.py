eb67c68c209b0e2ce32d07984ece875fa0eef947
Salman Badr <mailsbadr@gmail.com>
2026-06-19 12:30:00 -0400

    5: added temperature

--- src/chat.py
+++ src/chat.py
@@ -5,17 +5,23 @@ from openai import OpenAI, RateLimitError
 
 load_dotenv()
 persona = "You are a pirate"
+developer_message = [
+    {
+        "role": "developer",
+        "content": persona
+    },
 
+]
 
-def chat_using_responses(client, input_history, prompt):
+def chat_using_responses(client, message, prompt):
     try:
-        input_history.append({
+        message.append({
             "role": "user",
             "content": prompt
         })
         response = client.responses.create(
             model="gpt-5.4-nano",
-            input=input_history,
+            input=message,
             store=False,
             include=["reasoning.encrypted_content"],
         )
@@ -24,29 +30,25 @@ def chat_using_responses(client, input_history, prompt):
         print(f"There was a problem {e}")
     else:
         print(response.output_text)
-        input_history += response.output
+        message += response.output
 
 
-def chat_using_conversations(client, prompt, conversation_id):
+def chat_using_conversations(client, prompt, conversation_id, temperature):
     try:
         if conversation_id == '':
-            conversation = client.conversations.create(items=[
-                {
-                    "role": "developer",
-                    "content": persona
-                },
-            ])
+            conversation = client.conversations.create(items=developer_message)
             conversation_id = conversation.id
 
-        messages = [{
+        message = [{
             "role": "user",
             "content": prompt
         }]
 
         response = client.responses.create(
             model="gpt-5.4-nano",
-            input=messages,
-            conversation=conversation_id
+            input=message,
+            conversation=conversation_id,
+            temperature=float(temperature)
         )
     except RateLimitError as e:
         print(f"There was a problem {e}")
@@ -57,22 +59,17 @@ def chat_using_conversations(client, prompt, conversation_id):
 
 def main():
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-    input_history = [
-        {
-            "role": "developer",
-            "content": persona
-        },
-
-    ]
     conversation_id = ''
+
     while True:
 
         prompt = input(
             "\nEnter nutritional advice that you seek or type END to end conversation:\n")
 
         if prompt.upper() != 'END':
+            temperature = input("Enter temperature between 0 and 2: ")
             conversation_id = chat_using_conversations(
-                client=client, prompt=prompt, conversation_id=conversation_id)
+                client=client, prompt=prompt, conversation_id=conversation_id, temperature=temperature)
         else:
             break
 
