e47f57b21439d118430cd2d12a789078fd8d0bc9
Salman Badr <mailsbadr@gmail.com>
2026-06-20 16:44:59 -0400

    7: used an english teacher persona

--- src/chat.py
+++ src/chat.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv
 from openai import AsyncOpenAI, RateLimitError
 
 load_dotenv()
-persona = "You are a pirate"
+persona = "You are an english teacher whose purpose is to help students from grade 4 to grade 8" \
+    "in the Ontario region of Canada with improving reading and comprehension skills." \
+    "The conversation will start off with the student/user providing the genre that they will like to read from " \
+    "As such, you are to provide them with a small passage, consisting of 3 paragraphs, to read and then ask them one multiple choice question about the passage. "\
+    "After the student responds then you will ask another multiple choice question until the interaction ends." \
+    "You will then provide feedback on their answers and suggest ways to improve their reading and comprehension skills."
+
 developer_message = [
     {
         "role": "developer",
@@ -37,7 +43,7 @@ async def chat_using_responses(client, message, prompt):
         message += response.output
 
 
-async def chat_using_conversations(client, prompt, conversation_id, temperature):
+async def chat_using_conversations(client, prompt, conversation_id):
     try:
         if conversation_id == '':
             conversation = await client.conversations.create(items=developer_message)
@@ -52,7 +58,6 @@ async def chat_using_conversations(client, prompt, conversation_id, temperature)
             model="gpt-5.4-nano",
             input=message,
             conversation=conversation_id,
-            temperature=float(temperature)
         )
     except RateLimitError as e:
         print(f"There was a problem {e}")
@@ -70,43 +75,43 @@ async def chat_using_conversations(client, prompt, conversation_id, temperature)
         }
 
 
+def print_statistics(usage):
+    input_tokens, output_tokens, total_tokens = usage[
+        "input_tokens"], usage["output_tokens"],  usage['total_tokens']
+
+    max_tokens = 1_000_000   # 1 million tokens
+    input_price = 0.20  # input token costs
+    output_price = 1.25  # output token costs
+
+    cost_incurred = (input_tokens * input_price +
+                     output_tokens * output_price) / max_tokens
+
+    print("\nUSAGE STATISTICS")
+    print(f"The input token count is {input_tokens}")
+    print(f"The output token count is {output_tokens}")
+    print(f"The total token count is {total_tokens}")
+
+    print(f"Rough cost estimate is ${cost_incurred:.6f}")
+
+
 async def main():
     client = AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
     conversation_id = ''
 
-    while True:
-        prompt = input(
-            "\nEnter nutritional advice that you seek or type END to end conversation:\n")
+    prompt = input(
+        "\nEnter the genre that you will like to read. Type END to end conversation:\n")
+    while prompt.upper() != 'END':
 
-        if prompt.upper() != 'END':
-            temperature = input("Enter temperature between 0 and 2: ")
-            response = await chat_using_conversations(
-                client=client, prompt=prompt, conversation_id=conversation_id, temperature=temperature)
+        response = await chat_using_conversations(
+            client=client, prompt=prompt, conversation_id=conversation_id)
 
-            conversation_id = response.get('conversation_id')
-            output_text = response.get('output_text')
-            usage = response.get('usage')
-            input_tokens, output_tokens, total_tokens = usage[
-                "input_tokens"], usage["output_tokens"],  usage['total_tokens']
+        conversation_id = response.get('conversation_id')
+        output_text = response.get('output_text')
+        usage = response.get('usage')
+        print(f"\n{output_text}")
+        print_statistics(usage=usage)
 
-            max_tokens = 1_000_000   # 1 million tokens
-            input_price = 0.20  # input token costs
-            output_price = 1.25  # output token costs
-
-            cost_incurred = (input_tokens * input_price +
-                             output_tokens * output_price) / max_tokens
-
-            print(f"\n{output_text}")
-            print("\nUSAGE STATISTICS")
-            print(f"The input token count is {input_tokens}")
-            print(f"The output token count is {output_tokens}")
-            print(f"The total token count is {total_tokens}")
-
-            print(f"Rough cost estimate is ${cost_incurred:.6f}")
-
-        else:
-            break
-
+        prompt = input("\nEnter response or type END to end conversation:\n")
     print("Thanks for using OpenAI chat")
 
 
